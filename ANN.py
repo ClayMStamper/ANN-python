e34909b6f7eff2669586bb2830786e04a07a1ae2
clayton <claystamper@gmail.com>
2019-11-26 12:28:26 -0600

    data preprocessing done

--- ANN.py
+++ ANN.py
@@ -2,19 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modeling.csv')
-X = dataset.iloc[:, [2,3]].values
-y = dataset.iloc[:, 4].values
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# encode categorical data
+countryEncoder = genderEncoder = LabelEncoder()
+X[:, 1] = countryEncoder.fit_transform(X[:, 1])
+X[:, 2] = genderEncoder.fit_transform(X[:, 2])
+hotEncoder = OneHotEncoder(categorical_features=[1])
+X = hotEncoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+# Split data into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Feature scaling
+# method for normalizing the range of features (X variables)
+scalar = StandardScaler()
+X_train = scalar.fit_transform(X_train)  # calculate mean and std_dev then scale
+X_test = scalar.transform(X_test)  # just scale
 
 
-#Split data into training set and test set
-from sklearn.model_selection import train_test_split
-inputTrain, inputTest, outputTrain, outputTest = train_test_split(X, y, test_size=0.25, random_state=0)
-
-#Feature scaling
-#method for normalizing the range of features (X variables)
-from sklearn.preprocessing import StandardScaler
-inputScalar = StandardScaler()
-# inputTrain = inputScalar.fit_transform(inputTrain) #calculate mean and std_dev then scale
-# inputTest = inputScalar.transform(inputTest) # just scale
+ann = Sequential()
